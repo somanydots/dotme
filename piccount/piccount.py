493f4b2805ce2cc5b9e404e11f660167929a4ccc
Steven Burt <steve@planet.com>
2014-07-18 23:09:16 +0000

    Break out auth

--- piccount/piccount.py
+++ piccount/piccount.py
@@ -4,27 +4,39 @@
 
 import serial
 import requests
-
+import json
 from time import sleep, time
 
-PORT = "/dev/ttyACM1"
+PORT = "/dev/ttyACM0"
 SPEED = 38400
 URL = "xxx"
+AUTH_FILE = "auth.json"
 
 
 def main():
-    ser = serial.Serial(PORT, SPEED)
+    with open(AUTH_FILE, "r") as f:
+        auth = json.loads(f.read())
+    print auth
+    auth_tuple = (auth['user'], auth['pass'])
 
+    ser = serial.Serial(PORT, SPEED)
+    sleep(5)
+    ser.write(" \n")
     last_count = 0
+    sleeptime = 1
     while True:
         count = requests.get(URL,
-                             auth=("xxx", "xxx")).json()['count']
-        print time(), count, last_count
+                             auth=auth_tuple).json()['count']
+        print time(), count, last_count, sleeptime
+        if count != last_count:
+            ser.write("SPACE CA$H\n")
+            sleep(.25)
         ser.write('PICS: %i\n' % count)
         if count == last_count:
-            sleep(60)
+            sleeptime = 60 if sleeptime > 60 else sleeptime * 2
         else:
-            sleep(5)
+            sleeptime = 1
+        sleep(sleeptime)
 
         last_count = count
 
